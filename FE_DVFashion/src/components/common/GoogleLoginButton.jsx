7c1f4c92be7a19556ec2903ef244f5a86927bdb3
Duong-GB <duongnguyenqn1323@gmail.com>
2025-09-09 16:48:01 +0700

    [update] Header, ForgotPasswordForm, LoginForm, RegisterForm for translation

--- FE_DVFashion/src/components/common/GoogleLoginButton.jsx
+++ FE_DVFashion/src/components/common/GoogleLoginButton.jsx
@@ -1,7 +1,23 @@
 import { API_BASE_URL } from "../../services/api";
 ("../../services/api");
+import { useTranslation } from "react-i18next";
+import { useEffect } from "react";
 
 const GoogleLoginButton = () => {
+  const { t, i18n } = useTranslation();
+
+  useEffect(() => {
+    const handleLanguageChange = () => {
+      // Force component update
+    };
+
+    i18n.on("languageChanged", handleLanguageChange);
+
+    return () => {
+      i18n.off("languageChanged", handleLanguageChange);
+    };
+  }, [i18n]);
+
   const handleGoogleLogin = () => {
     // Redirect to backend OAuth2 endpoint
     window.location.href = `${API_BASE_URL}/oauth2/authorization/google`;
@@ -13,7 +29,7 @@ const GoogleLoginButton = () => {
         onClick={handleGoogleLogin}
         className="w-full bg-red-500 text-white py-2 px-4 rounded-lg font-semibold hover:bg-red-600 transition disabled:opacity-50"
       >
-        Đăng nhập với Google
+        {t("auth.google_login")}
       </button>
     </div>
   );
